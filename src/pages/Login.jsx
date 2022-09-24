69327032ea2f1ab439766d6a1fb802e7f5ff637c
Inderpreet02 <inder192002@gmail.com>
2022-09-24 16:19:53 +0530

    progress made

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../redux/apiCalls'
 import './Login.css'
 
 const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
+
+    const handleClick = (e) => {
+
+        e.preventDefault();
+
+        login(dispatch, {username, password});
+    }
+
+
   return (
     <div className='login__container'>
         <div className="login__wrapper">
             <div className="login__title">CREATE ACCOUNT</div>
             <form className="login__form">
-                <input type="text" placeholder='name' className="login__input" />
-                <input type="text" placeholder='password' className="login__input" />
+                <input type="text" placeholder='name' className="login__input" onChange={(e) => setUsername(e.target.value)}/>
+                <input type="text" placeholder='password' className="login__input"  onChange={(e) => setPassword(e.target.value)}/>
                 <div className="login__agreement">
                     By Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet. <b>PRIVACY POLICY</b>
                 </div>
-                <button className="login__btn">
+                <button className="login__btn" onClick={handleClick}>
                     Create
                 </button>
             </form>
